09c9c589084e33b80a4b97627c620b1661bb4408
sandra <sandra.h.arriaga@gmail.com>
2019-11-27 22:28:11 -0600

    add baseGuitar class declaration

--- Instruments.playground/Contents.swift
+++ Instruments.playground/Contents.swift
@@ -147,5 +147,47 @@ class Amplifier {
     }
 }
 
+// 1
+class ElectricGuitar: Guitar {
+    // 2
+    let amplifier: Amplifier
+    
+    // 3
+    init(brand: String, stringGauge: String = "light", amplifier: Amplifier) {
+        self.amplifier = amplifier
+        super.init(brand: brand, stringGauge: stringGauge)
+    }
+    
+    // 4
+    override func tune() -> String {
+        amplifier.plugIn()
+        amplifier.volume = 5
+        return "Tune \(brand) electric with E A D G B E"
+    }
+    
+    // 5
+    override func play(_ music: Music) -> String {
+        let preparedNotes = super.play(music)
+        return "Play solo \(preparedNotes) at volume \(amplifier.volume)."
+    }
+}
 
 
+class BassGuitar: Guitar {
+    let amplifier: Amplifier
+    
+    init(brand: String, stringGauge: String = "heavy", amplifier: Amplifier) {
+        self.amplifier = amplifier
+        super.init(brand: brand, stringGauge: stringGauge)
+    }
+    
+    override func tune() -> String {
+        amplifier.plugIn()
+        return "Tune \(brand) bass with E A D G"
+    }
+    
+    override func play(_ music: Music) -> String {
+        let preparedNotes = super.play(music)
+        return "Play bass line \(preparedNotes) at volume \(amplifier.volume)."
+    }
+}
