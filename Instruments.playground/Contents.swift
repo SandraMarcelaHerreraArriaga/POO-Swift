5d1b7e49a7f4ae113477251251c4c95a3787d4e7
sandra <sandra.h.arriaga@gmail.com>
2019-11-27 20:44:33 -0600

    add Piano class as example of using inheritance

--- Instruments.playground/Contents.swift
+++ Instruments.playground/Contents.swift
@@ -29,3 +29,28 @@ class Music{
         return notes.joined(separator:  "")
     }
 }
+/*
+ Inheritance
+ */
+//Piano as a subclass of the Instrument parent class. All stored properties and methods are automatically inherited by the Piano child class and available for use
+class Piano : Instrument{
+    let hasPedals: Bool
+    //the associated values of their corresponding properties don't change dinamycally. Static keyword reflect this
+    static let whiteKeys = 52
+    static let blackKeys = 36
+    
+    init(brand:String, hasPedals: Bool = false) {
+        self.hasPedals = hasPedals
+        //super keyword to call the parent class initializer after setting the child class
+        super.init(brand: brand)
+    }
+    //override the ingerited tune method's implementation. This provides an implementation of tune() that doesn't call fatalError() but rather does something specific to Piano
+    override func tune() -> String {
+        return "Piano standar tuning for \(brand). "
+    }
+    
+    override func play(_ music: Music) -> String {
+        let preparedNotes = super.play(music)
+        return "Piano playing \(preparedNotes)"
+    }
+}
